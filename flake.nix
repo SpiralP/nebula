8de852a52a20decb68b5869a03fb3498ef95b337
SpiralP <spiral.palmer@gmail.com>
2024-09-09 15:54:29 -0700

    add nix flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+
+      makePackages = (pkgs: {
+        default = pkgs.buildGoModule rec {
+          pname = "nebula";
+          version = "1.9.4-custom-${self.shortRev or self.dirtyShortRev}";
+
+          src = lib.sourceFilesBySuffices ./. [ ".go" ".mod" ".sum" ];
+
+          vendorHash = "sha256-oXhq+s5gDKPVClZpOzYi7BaYwcDqbCLBEO5BNGy9LJA=";
+
+          subPackages = [ "cmd/nebula" "cmd/nebula-cert" ];
+
+          ldflags = [ "-X main.Build=${version}" ];
+        };
+      }
+      );
+    in
+    builtins.foldl' lib.recursiveUpdate { } (builtins.map
+      (system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+          };
+
+          packages = makePackages pkgs;
+        in
+        {
+          devShells.${system} = packages;
+          packages.${system} = packages;
+        })
+      lib.systems.flakeExposed);
+}
